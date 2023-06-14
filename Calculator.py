9efa0728c6918daf7d38502b0295b5e7079c8b7c
Anjula <anjulajayasinghe100@gmail.com>
2023-06-15 00:39:20 +0530

    Updated the code

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,10 @@
-from tkinter import Tk, Entry, Button, StringVar
-
+from tkinter import *
 
 root = Tk()
+root.title("Simple Calculator | AJ")
+root.geometry("570x600+100+200")
+root.resizable(False, False)
+root.configure(bg="#17161b")
+
+
 root.mainloop()
